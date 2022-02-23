0f180425f1b7e712211797713027c1513965a786
Andrew J Hughes <andrew@ajhughes.dev>
2022-02-22 22:22:27 -0500

    con't work on modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,4 @@
 const dialog = document.getElementById('modal');
-const dialogContent = document.querySelector('.dialog-content');
-const h1 = document.querySelector('.modal-h1');
 const modalOpenBtn = document.querySelectorAll('#header-button, #hero-button');
 const closeBtn = document.querySelector('.close-button')
 
@@ -9,5 +7,3 @@ modalOpenBtn.forEach(btn => {
     dialog.showModal();
   })
 })
-
-closeBtn.addEventListener('close', function onClose() {});
