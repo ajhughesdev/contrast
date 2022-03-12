c0c7ecbc331d5cde5c26167cc6b62cb24340c490
Andrew J Hughes <andrew@ajhughes.dev>
2022-03-12 15:26:48 -0500

    finished tablet

--- js/fonts.js
+++ js/fonts.js
@@ -42,50 +42,50 @@ function fetchFonts() {
     });
 }
 
-function fetchMockups() {
-  let url = "../api/mockups/mockups.json";
+// function fetchMockups() {
+//   let url = "../api/mockups/mockups.json";
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let resources = data;
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       let resources = data;
 
-      resources.map(function (resource) {
-        let li = document.createElement("li");
-        let icon = document.createElement("img");
-        let title = document.createElement("h2");
-        let desc = document.createElement("p");
-        let link = document.createElement("a");
+//       resources.map(function (resource) {
+//         let li = document.createElement("li");
+//         let icon = document.createElement("img");
+//         let title = document.createElement("h2");
+//         let desc = document.createElement("p");
+//         let link = document.createElement("a");
 
-        title.innerHTML = `${resource.Title}`;
-        desc.innerHTML = `${resource.Description}`;
-        link.innerHTML = `Visit Resource →`;
+//         title.innerHTML = `${resource.Title}`;
+//         desc.innerHTML = `${resource.Description}`;
+//         link.innerHTML = `Visit Resource →`;
 
-        icon.setAttribute("src", "../api/mockups/" + `${resource.Icon}`);
-        icon.setAttribute("alt", "logo");
-        link.setAttribute("href", `${resource.Link}`);
+//         icon.setAttribute("src", "../api/mockups/" + `${resource.Icon}`);
+//         icon.setAttribute("alt", "logo");
+//         link.setAttribute("href", `${resource.Link}`);
 
-        li.appendChild(icon);
-        li.appendChild(title);
-        li.appendChild(desc);
-        li.appendChild(link);
-        list.appendChild(li);
+//         li.appendChild(icon);
+//         li.appendChild(title);
+//         li.appendChild(desc);
+//         li.appendChild(link);
+//         list.appendChild(li);
 
-        ul.appendChild(list);
+//         ul.appendChild(list);
 
-        ul.setAttribute("class", "resource-grid-cell");
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
+//         ul.setAttribute("class", "resource-grid-cell");
+//       });
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// }
 
 fetchFonts();
 
-function gridFonts() {
-  document.getElementById("mockups").addEventListener("click", fetchMockups());
-  document.getElementById("fonts").style.display = "none";
-}
+// function gridFonts() {
+//   document.getElementById("mockups").addEventListener("click", fetchMockups());
+//   document.getElementById("fonts").style.display = "none";
+// }
