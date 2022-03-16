50477d439a384ac39a61fee429997ed8d292bb44
Andrew J Hughes <andrew@ajhughes.dev>
2022-03-16 08:55:40 -0400

    always something to adjust!

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,8 @@ module.exports = {
     require("postcss-nesting"),
     require("postcss-nested"),
     require("autoprefixer"),
+    require("cssnano")({
+      preset: "default",
+    }),
   ],
 };
